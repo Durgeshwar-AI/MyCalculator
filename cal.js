2f10a5a7c3e80bb1ab7cd290123a6f57f46b2260
Durgeshwar-AI <durgesh34shaw94@gmail.com>
2024-01-12 03:20:36 +0530

    Fininsh

---
+++ cal.js
@@ -0,0 +1,61 @@
+var buttons = document.getElementsByClassName("values");
+var dec = document.getElementById("dec");
+dec.disabled = false;
+
+for (var i = 0; i < buttons.length; i++) {
+  buttons[i].onclick = click;
+}
+
+var operator = document.getElementsByClassName("operator");
+for (var i = 0; i < operator.length; i++) {
+  operator[i].onclick = clickOperator;
+}
+
+function click() {
+  var value = this.value;
+  var screen = document.getElementById("screen");
+  screen.innerText += value;
+  for (var i = 0; i < operator.length; i++) {
+    operator[i].disabled = false;
+  }
+  dec.disabled = false;
+}
+
+function clickOperator() {
+  var value = this.value;
+  var screen = document.getElementById("screen");
+  screen.innerText += value;
+  for (var i = 0; i < operator.length; i++) {
+    operator[i].disabled = true;
+  }
+  dec.disabled = false;
+}
+
+var clear = document.getElementById("clear");
+clear.onclick = function () {
+  window.location.reload();
+};
+
+var equal = document.getElementById("equal");
+equal.onclick = function () {
+  try {
+    var screen = document.getElementById("screen");
+    screen.innerText = eval(screen.innerText);
+    dec.disabled = false;
+  } catch {
+    screen.innerText = "Error";
+  }
+};
+
+var del = document.getElementById("del");
+del.addEventListener("click", () => {
+  var display = document.getElementById("screen");
+  if (display.innerText.charAt(display.innerText.length - 1) == ".") {
+    dec.disabled = false;
+  }
+  display.innerText = display.innerText.slice(0, -1);
+});
+
+dec.addEventListener("click", () => {
+  dec.disabled = true;
+});
